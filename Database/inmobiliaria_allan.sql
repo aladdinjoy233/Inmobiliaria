7c9c8afb6765f658c9c996bfd64e6453a3ea574a
Aladdin <ninjalover249@gmail.com>
2023-04-02 20:09:35 -0300

    Update database with new tables

--- Database/inmobiliaria_allan.sql
+++ Database/inmobiliaria_allan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 28, 2023 at 01:23 AM
+-- Generation Time: Apr 03, 2023 at 01:07 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -20,6 +20,54 @@ SET time_zone = "+00:00";
 --
 -- Database: `inmobiliaria`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contratos`
+--
+
+CREATE TABLE `contratos` (
+  `id_contrato` int(11) NOT NULL,
+  `id_inmueble` int(11) NOT NULL,
+  `id_inquilino` int(11) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `monto_mensual` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contratos`
+--
+
+INSERT INTO `contratos` (`id_contrato`, `id_inmueble`, `id_inquilino`, `fecha_inicio`, `fecha_fin`, `monto_mensual`) VALUES
+(2, 1, 4, '2023-04-03', '2023-04-05', '5000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inmuebles`
+--
+
+CREATE TABLE `inmuebles` (
+  `id_inmueble` int(11) NOT NULL,
+  `id_propietario` int(11) NOT NULL,
+  `direccion` varchar(45) NOT NULL,
+  `uso` varchar(45) NOT NULL,
+  `tipo` varchar(45) NOT NULL,
+  `ambientes` int(11) NOT NULL,
+  `latitud` decimal(10,0) NOT NULL,
+  `longitud` decimal(10,0) NOT NULL,
+  `precio` decimal(10,0) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `inmuebles`
+--
+
+INSERT INTO `inmuebles` (`id_inmueble`, `id_propietario`, `direccion`, `uso`, `tipo`, `ambientes`, `latitud`, `longitud`, `precio`, `activo`) VALUES
+(1, 1, 'Por alla', 'Hogar', 'Casa', 5, '10', '10', '5000', 1);
 
 -- --------------------------------------------------------
 
@@ -75,6 +123,21 @@ INSERT INTO `propietarios` (`id_propietario`, `dni`, `apellido`, `nombre`, `tele
 --
 
 --
+-- Indexes for table `contratos`
+--
+ALTER TABLE `contratos`
+  ADD PRIMARY KEY (`id_contrato`),
+  ADD KEY `id_inmueble` (`id_inmueble`),
+  ADD KEY `id_inquilino` (`id_inquilino`);
+
+--
+-- Indexes for table `inmuebles`
+--
+ALTER TABLE `inmuebles`
+  ADD PRIMARY KEY (`id_inmueble`),
+  ADD KEY `id_propietario` (`id_propietario`);
+
+--
 -- Indexes for table `inquilinos`
 --
 ALTER TABLE `inquilinos`
@@ -93,6 +156,18 @@ ALTER TABLE `propietarios`
 --
 
 --
+-- AUTO_INCREMENT for table `contratos`
+--
+ALTER TABLE `contratos`
+  MODIFY `id_contrato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `inmuebles`
+--
+ALTER TABLE `inmuebles`
+  MODIFY `id_inmueble` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `inquilinos`
 --
 ALTER TABLE `inquilinos`
@@ -103,6 +178,23 @@ ALTER TABLE `inquilinos`
 --
 ALTER TABLE `propietarios`
   MODIFY `id_propietario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `contratos`
+--
+ALTER TABLE `contratos`
+  ADD CONSTRAINT `contratos_ibfk_1` FOREIGN KEY (`id_inmueble`) REFERENCES `inmuebles` (`id_inmueble`),
+  ADD CONSTRAINT `contratos_ibfk_2` FOREIGN KEY (`id_inquilino`) REFERENCES `inquilinos` (`id_inquilino`);
+
+--
+-- Constraints for table `inmuebles`
+--
+ALTER TABLE `inmuebles`
+  ADD CONSTRAINT `inmuebles_ibfk_1` FOREIGN KEY (`id_propietario`) REFERENCES `propietarios` (`id_propietario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
